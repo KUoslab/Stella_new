9562a7aaf16797fd0a17acb80210f2dd31cce246
suwanKim <asfaca@naver.com>
2018-07-23 11:57:16 +0900

    Modify CPU utilization threshold converting quota to -1 under circumstance where I/O execution finish

--- module/gos/gos.h
+++ module/gos/gos.h
@@ -45,7 +45,7 @@
 #define SYS_CPU_UTIL_THRESHOLD 8500 /* 8500 = 85.00% */
 #define WORK_CONSERVING -1
 #define SLA_GOAL 10000
-#define EXIT_CPU_UTIL 1000
+#define EXIT_CPU_UTIL 200 /* 2% */
 #define PERIOD 100000	/* us, 100ms */
 #define SYS_CPU_MAX_UTIL 9800 /* 98.00% */
 
