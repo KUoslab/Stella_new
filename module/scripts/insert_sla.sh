d975f8c97818bb1db1a8f06fade0afeb9a729484
suwanKim <asfaca@naver.com>
2018-10-10 18:31:56 +0900

    Modify sla insertion script - get vhost pid

--- module/scripts/insert_sla.sh
+++ module/scripts/insert_sla.sh
@@ -2,6 +2,7 @@ vm=$1
 sla_option=$2
 sla_value=$3
 
+count=`expr 0`
 pid=$(sudo grep domstatus /var/run/libvirt/qemu/${vm}.xml | tr -dc '0-9')
 dev_string=$(virsh dumpxml ${vm} | egrep 'nvme|sd')
 name_dev=${dev_string#*\'}
@@ -12,9 +13,18 @@ if [ ! "${name_dev}"];then
 fi
 
 if [ "$2" = "n_maxcredit" ];then
-	pid=`expr ${pid} + 2`
+	vhost_cmd=$(ps -el | grep vhost | grep ${pid})
+	for vhost_pid in ${vhost_cmd}
+	do
+		count=`expr ${count} + 1`
+
+		if [ ${count} = 4 ];then
+			pid=${vhost_pid}
+		fi
+	done
+	#check this command line
+	#name_dev="null"
 fi
 
 cmd="$1 $2 $3 ${pid} ${name_dev}"
-
-echo ${cmd} #> /proc/gos_vm_info
+echo ${cmd} > /proc/gos_vm_info
