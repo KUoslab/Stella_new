3adde9e4ecc4dd27ea094d0e64d0b7639f759628
suwanKim <asfaca@naver.com>
2018-10-17 12:13:09 +0900

    Modify scripts

---
+++ module/scripts/insert_sla_tta.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo sh insert_sla.sh $1 n_maxcredit 2000
+sudo sh insert_sla.sh $2 b_bw 300000
--- module/scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#insert vm name, sla option and sla value for users
-./insert_target_vm.sh vm_name1 sla_option sla_value
-./insert_target_vm.sh vm_name2 sla_option sla_value
-./insert_target_vm.sh vm_name3 sla_option sla_value
-
