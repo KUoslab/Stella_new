a0ec87a8a4c34319b4b64ca005da009662792992
may21 <may21.lee@gmail.com>
2018-12-12 15:35:58 +0900

    Modify script

--- module/scripts/insert_sla.sh
+++ module/scripts/insert_sla.sh
@@ -33,11 +33,7 @@ vm_path2=$(sudo ls ${vm_path} | grep ${vm})
 vm_path="${vm_path}${vm_path2}"
 
 vcpu_string=$(virsh dumpxml ${vm} | grep nova:vcpu | grep -v grep | cut -d">" -f2)
-num_vcpu=${vcpustring%<*}
-
-#vcpu_string=$(virsh dumpxml ${vm} | grep vcpu | grep -v grep)
-#num_vcpu=${vcpu_string#*>}
-#num_vcpu=${num_vcpu%<*}
+num_vcpu=${vcpu_string%<*}
 
 for i in $(seq 0 $((num_vcpu - 1)))
 do
