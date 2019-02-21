710496b11c3b391712b1cbe03d48aea49120ca5c
may21 <may21.lee@gmail.com>
2019-02-21 17:21:37 +0900

    Modify insert script for finding vcpu pids

--- module/scripts/insert_sla.sh
+++ module/scripts/insert_sla.sh
@@ -28,20 +28,10 @@ if [ "$2" = "n_maxcredit" ];then
 	#name_dev="null"
 fi
 
-vm_path="/sys/fs/cgroup/cpu/machine/${vm}.libvirt-qemu"
-vcpu_string=$(virsh dumpxml ${vm} | grep vcpu | grep -v grep)
-num_vcpu=${vcpu_string#*>}
-num_vcpu=${num_vcpu%<*}
-
-for i in $(seq 0 $((num_vcpu - 1)))
-do
-	vcpu_path="${vm_path}/vcpu$i"
-
-	pid2="${pid2} `cat "${vcpu_path}/tasks"`"
-done
+vcpu=$(sudo cat /var/run/libvirt/qemu/${vm}.xml | grep vcpu | grep pid | awk '{print $3}' | sed 's/[^0-9]//g')
 
 if [ "$2" = "c_usage" ];then
-	cmd="$1 $2 $3 ${pid2} ${name_dev}"
+	cmd="$1 $2 $3 ${vcpu} ${name_dev}"
 else
 	cmd="$1 $2 $3 ${pid} ${name_dev}"
 fi
