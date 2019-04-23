9d7d13e6a2e86a31e850d7139cdefa98aa869f68
Suwan Kim <asfaca@naver.com>
2019-04-23 17:34:35 +0900

    change script for CPU SLO

--- module/scripts/insert_sla.sh
+++ module/scripts/insert_sla.sh
@@ -28,12 +28,12 @@ if [ "$2" = "n_maxcredit" ];then
 	#name_dev="null"
 fi
 
-vcpu=$(sudo cat /var/run/libvirt/qemu/${vm}.xml | grep vcpu | grep pid | awk '{print $3}' | sed 's/[^0-9]//g')
+vcpu=$(sudo cat /var/run/libvirt/qemu/${vm}.xml | grep vcpu | grep pid | sed 's/[^0-9]//g')
 
 if [ "$2" = "c_usage" ];then
 	cmd="$1 $2 $3 ${vcpu} ${name_dev}"
 else
 	cmd="$1 $2 $3 ${pid} ${name_dev}"
 fi
-
+echo "$vcpu"
 echo ${cmd} > /proc/gos_vm_info
