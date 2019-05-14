6d470cd93e00181981c671994641a20cc3e7f6dd
Suwan Kim <asfaca@naver.com>
2019-05-14 02:42:55 -0700

    Solve compile error

--- module/gos.c
+++ module/gos.c
@@ -265,7 +265,7 @@ static int gos_vm_info_show(struct seq_file *m, void *v)
 				int_sla = curr_sla->now_sla / 100;
 				flt_sla = curr_sla->now_sla % 100;
 				sla_option = curr_sla->sla_option;
-				cpu_quota = curr_sla->cpu_quota;
+				cpu_quota = curr_sla->now_quota;
 				iops = gos_vm_list[i]->now_perf.iops;
 				bandwidth = gos_vm_list[i]->now_perf.bandwidth;
 				latency = gos_vm_list[i]->now_perf.latency;
