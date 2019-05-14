2c422c03461582d737dabcd14f26d261e2526ccd
Suwan Kim <asfaca@naver.com>
2019-05-14 02:37:27 -0700

    Assignment #2 - Suwan Kim

--- module/gos.c
+++ module/gos.c
@@ -235,8 +235,11 @@ static int gos_vm_info_show(struct seq_file *m, void *v)
 	int int_sla, flt_sla;
 	char *vm_name, *sla_option;
 	int i = 0;
+	long cpu_quota;
+	unsigned long iops, bandwidth, latency, pps;
+
 	
-	seq_puts(m, "VM_NAME\tSLO Option\tSLO Value\tSLO Percentage\n");
+	seq_puts(m, "VM_NAME\tSLO Option\tSLO Value\tSLO Percentage\tCPU quota\tDisk-IOPS\tDisk-Bandwidth\tDisk-Latency\tPPS\n");
 
 	for(i = 0 ; i < VM_NUM ; i++)
 	{
@@ -262,9 +265,14 @@ static int gos_vm_info_show(struct seq_file *m, void *v)
 				int_sla = curr_sla->now_sla / 100;
 				flt_sla = curr_sla->now_sla % 100;
 				sla_option = curr_sla->sla_option;
+				cpu_quota = curr_sla->cpu_quota;
+				iops = gos_vm_list[i]->now_perf.iops;
+				bandwidth = gos_vm_list[i]->now_perf.bandwidth;
+				latency = gos_vm_list[i]->now_perf.latency;
+				pps = gos_vm_list[i]->now_perf.credit;
 
-				seq_printf(m, "%s\t%s\t%d\t%d.%d\n", vm_name, sla_option, 
-					sla_value, int_sla, flt_sla);
+				seq_printf(m, "%s\t%s\t%d\t%d.%d\t%d\t%lu\t%lu\t%lu\t%lu\n", vm_name, sla_option,
+					sla_value, int_sla, flt_sla, cpu_quota, iops, bandwidth, latency, pps);
 
 
 			}
