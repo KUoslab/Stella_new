97c424c8a99153fc6b93297d3d2f8990108d7bda
may21 <may21.lee@gmail.com>
2019-05-20 07:55:32 +0000

    change source code tree

--- module/scripts/new_insert_sla.sh
+++ module/scripts/new_insert_sla.sh
@@ -28,13 +28,22 @@ if [ "$2" = "n_maxcredit" ];then
 	#name_dev="null"
 fi
 
-vcpu=$(sudo cat /var/run/libvirt/qemu/${vm}.xml | grep vcpu | grep pid | sed 's/[^0-9]//g')
-#vcpu=$(sudo cat /var/run/libvirt/qemu/${vm}.xml | grep vcpu | grep pid | awk '{print $3}' | sed 's/[^0-9]//g')
+vm_path="/sys/fs/cgroup/cpu/machine/${vm}.libvirt-qemu"
+vcpu_string=$(virsh dumpxml ${vm} | grep vcpu | grep -v grep)
+num_vcpu=${vcpu_string#*>}
+num_vcpu=${num_vcpu%<*}
+
+for i in $(seq 0 $((num_vcpu - 1)))
+do
+	vcpu_path="${vm_path}/vcpu$i"
+
+	pid2="${pid2} `cat "${vcpu_path}/tasks"`"
+done
 
 if [ "$2" = "c_usage" ];then
-	cmd="$1 $2 $3 ${vcpu} ${name_dev}"
+	cmd="$1 $2 $3 ${pid2} ${name_dev}"
 else
 	cmd="$1 $2 $3 ${pid} ${name_dev}"
 fi
-#echo "${cmd}"
+echo "${cmd}"
 echo ${cmd} > /proc/gos_vm_info
